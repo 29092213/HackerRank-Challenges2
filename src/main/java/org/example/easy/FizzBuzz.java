ec89fac56d396f537a736f2311e05e7550c4bbf8
Luiz Souza <luizrgb13@gmail.com>
2025-02-06 13:40:37 -0300

    ADD FIzzBuzz(Atualização)

--- src/main/java/org/example/easy/FizzBuzz.java
+++ src/main/java/org/example/easy/FizzBuzz.java
@@ -1,4 +1,5 @@
 package org.example.easy;
+import java.util.Scanner;
 //Escreva um programa curto em Java que imprime os números de 1 a 100, um por linha.
 
 //- Para múltiplos de `3`, imprima `"Fizz"` em vez do número.
@@ -11,12 +12,20 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 15; i++){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 até 100");
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= number; i++){
 
             if (i % 3 == 0 && i % 5 == 00) System.out.println("FizzBuzz");
             else if (i % 5 == 0 ) System.out.println("Buzz");
             else if (i % 3 == 0) System.out.println("Fizz");
             else System.out.println(i);
+
         }
+
+        sc.close();
     }
 }
